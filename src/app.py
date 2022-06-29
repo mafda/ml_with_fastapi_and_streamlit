0d0fddf1dfba8a5110cf2e516fcf14133abdd3a2
fernanda rodriguez <mafda@users.noreply.github.com>
2022-06-29 20:14:52 -0300

    create main function

--- src/app.py
+++ src/app.py
@@ -15,39 +15,49 @@ IMG_WIDTH = 28
 IMG_HEIGHT = 28
 MODEL = "./my_model/mnist.h5"
 
-st.set_page_config(page_title="mnist Prediction", page_icon=":1234:")
 
-st.header("Prototyping a ML Application")
-st.title("mnist Prediction")
-st.write("Draw something here")
+def main():
+    """Main Streamlit function
+    Read an image and show a probability
+    """
 
-# Create a canvas component
-canvas_image = st_canvas(
-    fill_color="black",
-    stroke_width=20,
-    stroke_color="gray",
-    width=CANVAS_SIZE,
-    height=CANVAS_SIZE,
-    drawing_mode="freedraw",
-    key="canvas",
-    display_toolbar=True,
-)
+    st.set_page_config(page_title="mnist Prediction", page_icon=":1234:")
 
-if canvas_image.image_data is not None:
-    # Scale down image to the model input size
-    img = cv2.resize(
-        np.uint8(canvas_image.image_data), (IMG_WIDTH, IMG_HEIGHT)
+    st.header("Prototyping a ML Application")
+    st.title("mnist Prediction")
+    st.write("Draw something here")
+
+    # Create a canvas component
+    canvas_image = st_canvas(
+        fill_color="black",
+        stroke_width=20,
+        stroke_color="gray",
+        width=CANVAS_SIZE,
+        height=CANVAS_SIZE,
+        drawing_mode="freedraw",
+        key="canvas",
+        display_toolbar=True,
     )
-    # Rescaled image upwards to show
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    if st.button("Predict"):
-        predict_class = predict_model(MODEL, img)
+    if canvas_image.image_data is not None:
+        # Scale down image to the model input size
+        img = cv2.resize(
+            np.uint8(canvas_image.image_data), (IMG_WIDTH, IMG_HEIGHT)
+        )
+        # Rescaled image upwards to show
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        predict_class = predict_class.numpy()
+        if st.button("Predict"):
+            predict_class = predict_model(MODEL, img)
 
-        probability = np.amax(predict_class)
-        number = np.where(predict_class == np.amax(predict_class))
+            predict_class = predict_class.numpy()
 
-        st.write("Number Predict:", str(np.amax(number)))
-        st.write("Probability:", str(probability))
+            probability = np.amax(predict_class)
+            number = np.where(predict_class == np.amax(predict_class))
+
+            st.write("Number Predict:", str(np.amax(number)))
+            st.write("Probability:", str(probability))
+
+
+if __name__ == "__main__":
+    main()
